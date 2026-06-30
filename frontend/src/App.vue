b94756c1419cb7d53591dd72fdc51f22403a5846
Manuel Schöne <schoene-manuel@gmx.de>
2026-07-01 00:30:26 +0200

    build: Updated Vue frontend and added packages
    Updated the frontend using newer TS configuration and package versions. Installed TailwindCSS, Lucide Icons and Google Sans font. Removed files from the Wails starter template. Switched JS runtime from Node.js to Bun.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,5 @@
 <script lang="ts" setup></script>
 
 <template>
-  <img id="logo" alt="Wails logo" src="./assets/images/logo-universal.png" />
+  <p>Hello World!</p>
 </template>
-
-<style>
-#logo {
-  display: block;
-  width: 50%;
-  height: 50%;
-  margin: auto;
-  padding: 10% 0 0;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  background-origin: content-box;
-}
-</style>
